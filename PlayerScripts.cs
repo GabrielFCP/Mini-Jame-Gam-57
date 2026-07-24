dc67fe2ff8fb6f878f6b804501755fa91b08234d
Felipoppops <felipebehalves@gmail.com>
2026-07-24 17:45:22 -0300

    player

---
+++ PlayerScripts.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class PlayerScripts : Node
+{
+	[Export]
+	RigidBody2D RB;
+	[Export]
+	AnimatedSprite2D Sprite;
+	[Export]
+	SpriteFrames Idle;
+	[Export]
+	SpriteFrames Run;
+	[Export]
+	SpriteFrames Attack;
+	Vector2 V2Input;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
