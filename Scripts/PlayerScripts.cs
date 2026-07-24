23e8400a1ac31e52afe95452268239bae6bd1ed9
Felipoppops <felipebehalves@gmail.com>
2026-07-24 19:24:55 -0300

    Pode morrer (Hit)

--- Scripts/PlayerScripts.cs
+++ Scripts/PlayerScripts.cs
@@ -15,6 +15,11 @@ public partial class PlayerScripts : Node
 	SpriteFrames Attack;
 	Vector2 V2Input;
 	float WalkSpeed = 400;
+	float MaxHP = 100;
+	float HP = 100;
+	double TSLH = -1;
+	bool IsAlive = true;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -23,7 +28,13 @@ public partial class PlayerScripts : Node
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Inputget();
+		if(IsAlive == true)
+		{
+			TSLH += delta;
+			Inputget();
+		}
+		if(HP <= 0)
+			IsAlive = false;
 	}
 
     public override void _PhysicsProcess(double delta)
@@ -39,5 +50,28 @@ public partial class PlayerScripts : Node
 	private void Movement()
 	{
 		RB.LinearVelocity = V2Input * WalkSpeed;
+	}
+
+	public void Hit(float Value, bool IsHeal = false)
+	{
+		if(IsHeal == true)
+		{
+			GD.Print($"Hit! {Value}");
+			HP -= Value;
+			HitTween();
+		}
+		else if(IsHeal == true)
+		{
+			GD.Print($"Healed! {Value}");
+			HP += Value;
+		}
+	}
+
+	private void HitTween()
+	{
+		Tween tween = CreateTween();
+		tween.SetLoops(4);
+		tween.TweenProperty(Sprite, "modulate:a", 0.2, 0.25);
+		tween.TweenProperty(Sprite, "modulate:a", 1, 0.25);
 	}
 }
