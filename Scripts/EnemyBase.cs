f1a20d97bb04f8f3579318a3e15fa8487e125bb5
Gabriel <gabriel_f_c_p@hotmail.com>
2026-07-26 00:16:37 -0300

    Added Ghost Animation helpers to the EnemyBaseScript.

--- Scripts/EnemyBase.cs
+++ Scripts/EnemyBase.cs
@@ -6,6 +6,8 @@ public abstract partial class EnemyBase : Node2D
 	[Export]
 	protected AnimatedSprite2D Sprite;
 	[Export]
+	protected Sprite2D Shadow;
+	[Export]
 	protected int Health;
 	[Export]
 	protected int MaxHealth = 100;
@@ -32,11 +34,80 @@ public abstract partial class EnemyBase : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		FloatTweenAnim();
+	}
+
+	#region AnimationHelpers
+
+	protected void FlipAnim()
+	{
+		if(RB.LinearVelocity.X > 0)
+			Sprite.FlipH = false;
+		else
+			Sprite.FlipH = true;
+	}
+
+
+	/// <summary>
+	/// Starts a float effect on the Ghost EnemyBase Sprite class variable. This is a Loop and should only be started once.
+	/// </summary>
+	protected void FloatTweenAnim()
+	{
+		Tween tween = CreateTween();
+		tween.SetLoops();
+		tween.SetEase(Tween.EaseType.Out);
+		tween.TweenProperty(Sprite, "position", new Vector2(Sprite.Position.X, -2.5f), 2f);
+		tween.TweenProperty(Sprite, "position", new Vector2(Sprite.Position.X, 2.5f), 2f);
+		tween.TweenInterval(0.1f);
+	}
+
+	/// <summary>
+	/// Creates a dissolve effect on both the EnemyBase Sprite and it's shadow.
+	/// </summary>
+	protected void DissolveTweenAnim()
+	{
+		Tween tween = CreateTween();
+		tween.TweenInterval(0.3f);
+		tween.SetEase(Tween.EaseType.OutIn);
+		tween.SetParallel();
+		tween.TweenProperty(Sprite, "modulate:a", 0, 2f);
+		tween.TweenProperty(Shadow, "modulate:a", 0, 2f);
+	}
+
+	protected int GhostCurrentSpriteIndex = 0;
+	/// <summary>
+	/// Whenever the enemy suffers damage, changing for the next frame will slowly make the ghost cleaner. Do not use it for death.
+	/// </summary>
+	protected void NextFrame()
+	{
+		int lenght = Sprite.SpriteFrames.GetFrameCount("default");
+		if(GhostCurrentSpriteIndex > lenght - 2) // Stop at the last clean frame. 
+		{
+			Sprite.Frame++;
+			GhostCurrentSpriteIndex++;
+		}
+	}
+
+	#endregion
+
+	/// <summary>
+	/// Can a Ghost die?
+	/// </summary>
+	protected void Death()
+	{
+		Sprite.Frame++;
+		DissolveTweenAnim();
+	}
+
+	public void TakeDamage2()
+	{
+		NextFrame();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		FlipAnim();
 		if(Target != null)
 		{
 			var TargetChildren = Target.GetChildren();
